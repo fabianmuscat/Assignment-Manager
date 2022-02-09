76f27e0dadb5fbb8f57e17c2cd39778aea4373fd
Fabian Muscat <fabian.muscat.d102385@mcast.edu.mt>
2022-02-09 09:39:54 +0100

    Fixed Hamburger Menu

--- App/Presentation/wwwroot/js/site.js
+++ App/Presentation/wwwroot/js/site.js
@@ -1,13 +1,26 @@
-﻿$(document).ready(function() {
-    $(document).delegate('.open', 'click', function(event){
-        $(this).addClass('oppenned');
-        event.stopPropagation();
+﻿function closeMenuOn(...selectors) {
+    $.each(selectors, (index, value) => {
+        $(document).on('click', value, function(event) {
+            $('.open').removeClass('opened').css('background-color', '#146c43');
+            $('.open span').css('position', 'relative');
+
+            $('.open span:nth-child(1)').css('top', '0');
+            $('.open span:nth-child(3)').css('top', '0');
+            event.stopPropagation();
+        });
     })
-    $(document).delegate('body', 'click', function() {
-        $('.open').removeClass('oppenned');
-    })
-    $(document).delegate('.cls', 'click', function(event){
-        $('.open').removeClass('oppenned');
+}
+
+$(document).ready(function() {
+    $(document).on('click', '.open', function(event){
+        $(this).addClass('opened');
+        $(this).css('background', 'none');
+        $('.open span').css('position', 'absolute');
+        
+        $('.open span:nth-child(1)').css('top', '15%');
+        $('.open span:nth-child(3)').css('top', '75%');
         event.stopPropagation();
     });
+    
+    closeMenuOn('body', '.opened', '.cls');
 });
